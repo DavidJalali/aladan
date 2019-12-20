bd92400268ffa39a43ed12c2750dc0def90c7f27
mb <m_bs85@yahoo.com>
2019-12-20 01:42:13 -0800

    edit

--- app/src/main/java/com/aladhan/MainActivity.java
+++ app/src/main/java/com/aladhan/MainActivity.java
@@ -8,7 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -33,7 +35,13 @@ public class MainActivity extends AppCompatActivity {
                     con.setRequestProperty("User-Agent", "Mozila/5.0");
                     int responseCode = con.getResponseCode();
                     if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                        String inputLine = null;
+                        StringBuffer response = new StringBuffer();
 
+                        while((inputLine = in.readLine()) != null){
+                            response.append(inputLine);
+                        }
                     }
                 } catch (MalformedURLException e) {
 
